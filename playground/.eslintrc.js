ee6e565313af2b675ea357acdc3d79c02855615e
kich555 <hyunjjang199582@gmail.com>
2022-05-29 03:59:08 +0900

    Initial setting

--- playground/.eslintrc.js
+++ playground/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: ['eslint:recommended', 'airbnb', 'plugin:prettier/recommended'],
   rules: {
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
-    'import/no-unresolved': 'warn',
+    'import/no-unresolved': 0,
     'no-unused-vars': 'warn',
   },
   // 위처럼 플러그인 이름 + / + rule 이름을 키로 하고 뒤는 공식문서에 나온 설명대로 원하는 규칙으로 설정해줍니다.
