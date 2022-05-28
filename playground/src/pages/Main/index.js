d30ce3d25e5fb21fc0757e19db539c50d678d94b
kich555 <hyunjjang199582@gmail.com>
2022-05-29 04:46:32 +0900

    Create Main page

---
+++ playground/src/pages/Main/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div className="main-page centered">
+      <ul className="list-wrapper relative">
+        <li className="list list-6 relative width-200 padding-16 link uppercase">Home</li>
+        <li className="list list-5 relative width-200 padding-16 link uppercase">About</li>
+        <li className="list list-4 relative width-200 padding-16 link uppercase">Services</li>
+        <li className="list list-3 relative width-200 padding-16 link uppercase">Portfolio</li>
+        <li className="list list-2 relative width-200 padding-16 link uppercase">Contract</li>
+        <li className="list list-1 relative width-200 padding-16 link uppercase">Git hub</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Main;
