21c799edaa29e4e6fc265227cf17bbeb0ffb7711
kich555 <hyunjjang199582@gmail.com>
2022-05-29 16:38:16 +0900

    Create DropDownList component

---
+++ playground/src/pages/Main/DropDownList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { capitalize } from 'lodash';
+
+function DropDownList({ navigateTo, idx }) {
+  const navigate = useNavigate();
+  const buttonName = capitalize(navigateTo);
+  const reverseIdx = idx - 6;
+  return (
+    <li className={`list list${reverseIdx} relative width-200 link uppercase`}>
+      <button className="max-width padding-16 text-left" type="button" onClick={() => navigate(`/${navigateTo}`)}>
+        {buttonName}
+      </button>
+    </li>
+  );
+}
+
+export default DropDownList;
