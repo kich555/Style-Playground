debcfc48a1c67e738de4a72fe01b90354e989f14
kich555 <hyunjjang199582@gmail.com>
2022-05-29 16:38:03 +0900

    turn off some rules in ESLint

--- playground/.eslintrc.js
+++ playground/.eslintrc.js
@@ -15,7 +15,8 @@ module.exports = {
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
     'import/no-unresolved': 0,
     'no-unused-vars': 'warn',
+    'react/prop-types': 0,
+    'eslint-plugin-import': 0,
     // Parsing error: Unexpected token import
   },
-  // 위처럼 플러그인 이름 + / + rule 이름을 키로 하고 뒤는 공식문서에 나온 설명대로 원하는 규칙으로 설정해줍니다.
 };
