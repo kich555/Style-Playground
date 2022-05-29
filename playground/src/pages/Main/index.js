c7b55a16e02707d92321b1a4843ececc2a169e88
kich555 <hyunjjang199582@gmail.com>
2022-05-29 16:38:38 +0900

    Apply DropDownList component in Main page component

--- playground/src/pages/Main/index.js
+++ playground/src/pages/Main/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import DropDownList from './DropDownList';
 
 function Main() {
+  const navigateList = ['home', 'about', 'animation', 'portfolio', 'contract', 'github'];
+
   return (
     <div className="main-page centered">
       <ul className="list-wrapper relative">
-        <li className="list list-6 relative width-200 padding-16 link uppercase">Home</li>
-        <li className="list list-5 relative width-200 padding-16 link uppercase">About</li>
-        <li className="list list-4 relative width-200 padding-16 link uppercase">Services</li>
-        <li className="list list-3 relative width-200 padding-16 link uppercase">Portfolio</li>
-        <li className="list list-2 relative width-200 padding-16 link uppercase">Contract</li>
-        <li className="list list-1 relative width-200 padding-16 link uppercase">Git hub</li>
+        {navigateList.map((list, idx) => (
+          <DropDownList navigateTo={`${list}`} idx={idx} />
+        ))}
       </ul>
     </div>
   );
