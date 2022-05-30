1cd8194f0280a0b991f2ab37efead5ad190f4053
kich555 <hyunjjang199582@gmail.com>
2022-05-30 18:20:37 +0900

    Create Animation page

--- playground/src/pages/Animation/index.js
+++ playground/src/pages/Animation/index.js
@@ -1,7 +1,18 @@
+import DropDownList from 'pages/Main/DropDownList';
 import React from 'react';
 
 function Animation() {
-  return <div>hello world</div>;
+  console.log('render?');
+  const navigateList = ['curveList', 'list-2', 'list-3', 'list-4', 'list-5', 'list-6'];
+  return (
+    <div className="animation-page centered">
+      <ul className="list-wrapper-3d relative">
+        {navigateList.map((list, idx) => (
+          <DropDownList name={`${list}`} navigateTo={`animation/detail/${list}`} idx={idx} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Animation;
