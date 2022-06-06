28f890485c982f5ee0bfacacc5c5d85ca1b835ad
kich555 <hyunjjang199582@gmail.com>
2022-06-06 23:43:59 +0900

    Refactoring CurveListItem componen

--- playground/src/components/CurveListItem.js
+++ playground/src/components/CurveListItem.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function CurveListItem({ isActive, iconName, handleClick, idx }) {
   return (
-    <li className="relative">
+    <li className={`relative ${isActive ? 'active' : ''}`}>
       <button onClick={() => handleClick(idx)} className="" type="button">
-        <span className={`icon ${isActive ? 'active' : ''} relative display-block width-56 height-56 text-center border-50 font-24`}>
+        <span className="icon  relative display-block width-56 height-56 text-center border-50 font-24">
           <ion-icon name={`${iconName}-outline`} />
         </span>
       </button>
